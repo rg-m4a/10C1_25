470432c2bbf5f95f86f9a1d1a1a6b632144b3f9e
rg-m4a <>
2026-02-09 10:35:59 +0100

    Grundgerüst Physik Minigame

---
+++ src/main/java/czg/objects/minigame_objects/PhysicsGoalObject.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package czg.objects.minigame_objects;
+
+import czg.objects.BaseObject;
+import czg.scenes.BaseScene;
+import java.awt.Image;
+
+/**
+ *
+ * @author guest-4zcdot
+ */
+public class PhysicsGoalObject extends BaseObject {
+     public PhysicsGoalObject(Image sprite, int x, int y) {
+        super(sprite, x, y);
+    }
+    
+    @Override
+    public void update(BaseScene scene) {}
+}
